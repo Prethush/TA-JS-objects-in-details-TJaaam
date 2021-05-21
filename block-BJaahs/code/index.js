aeda68a830bb5123915f1da65c4edca679bbaccb
prethush <ktmirash49@gmail.com>
2021-05-21 14:53:37 +0530

    OOP

--- block-BJaahs/code/index.js
+++ block-BJaahs/code/index.js
@@ -1,52 +1,44 @@
 //Without object
 
-// let title = "Where is the capital of Jordan";
-// let options = ["Tashkent", "Amaan", "Kuwait City", "Nairobi"];
-// let correctAnswerIndex = 1;
-
-// function isCorrectAnswer(index) {
-//     return index === correctAnswerIndex ? true : false;
-// }
-
-// function getCorrectAnswer() {
-//     return options[correctAnswerIndex];
-// }
+ let title = "Where is the capital of Jordan";
+ let options = ["Tashkent", "Amaan", "Kuwait City", "Nairobi"];
+ let correctAnswerIndex = 1;
+ function isCorrectAnswer(index) {
+     return index === correctAnswerIndex ? true : false;
+ }
+ function getCorrectAnswer() {
+     return options[correctAnswerIndex];
+ }
 
 //Using object
 
-// let obj = {
-//     title: "Where is the capital of Jordan",
-//     options: ["Tashkent", "Amaan", "Kuwait City", "Nairobi"],
-//     correctAnswerIndex: 1,
-
-//     isCorrectAnswer(index) {
-//         return obj.correctAnswerIndex === index ? true : false;
-//     },
-
-//     getCorrectAnswer() {
-//         return obj.options[obj.correctAnswerIndex];
-//     }
-// }
+ let obj = {
+     title: "Where is the capital of Jordan",
+     options: ["Tashkent", "Amaan", "Kuwait City", "Nairobi"],
+     correctAnswerIndex: 1,
+     isCorrectAnswer(index) {
+         return obj.correctAnswerIndex === index ? true : false;
+     },
+     getCorrectAnswer() {
+         return obj.options[obj.correctAnswerIndex];
+     }
+ }
 
 //Using function to create obj 
 
-// function quiz() {
-
-//     let obj = {};
-//     obj.title = "Where is the capital of Jordan";
-//     obj.options = ["Tashkent", "Amaan", "Kuwait City", "Nairobi"],
-//     obj.correctAnswerIndex = 1;
-
-//     obj.isCorrectAnswer = function(index) {
-//         return obj.correctAnswerIndex === index ? true : false;
-//     }
-
-//     obj.getCorrectAnswer = function() {
-//         return obj.options[obj.correctAnswerIndex];
-//     }
-
-//     return obj;
-// }
+ function quiz() {
+     let obj = {};
+     obj.title = "Where is the capital of Jordan";
+     obj.options = ["Tashkent", "Amaan", "Kuwait City", "Nairobi"],
+     obj.correctAnswerIndex = 1;
+     obj.isCorrectAnswer = function(index) {
+         return obj.correctAnswerIndex === index ? true : false;
+     }
+     obj.getCorrectAnswer = function() {
+         return obj.options[obj.correctAnswerIndex];
+     }
+     return obj;
+ }
 
 //Using this keyword
 
