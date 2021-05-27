f1769c2cfc9eafcb0a7fe470bbda065296223535
prethush <ktmirash49@gmail.com>
2021-05-27 18:18:15 +0530

    Quiz app

---
+++ block-BJaaim/data.js
@@ -0,0 +1,30 @@
+let questionArray = [
+
+    {
+        question: "Who is the President of India",
+        a: "Manmohan Singh",
+        b: "Amith Shah",
+        c: "Narendra Modi",
+        d: "Ramnath Kovind",
+        answer: "d"
+    },
+
+    {
+        question: "Who is the President of USA",
+        a: "M L K",
+        b: "J F K",
+        c: "Joe Biden",
+        d: "Donald Trump",
+        answer: "c"
+    },
+
+    {
+        question: "Who is the President of India",
+        a: "Manmohan Singh",
+        b: "Amith Shah",
+        c: "Narendra Modi",
+        d: "Ramnath Kovind",
+        answer: "d"
+    }
+
+]
